61fdade26c056bb633b2838ca5d0da67e62a0f6a
musashi.hadano <hadano.musashi.056@s.kyushu-u.ac.jp>
2024-10-19 00:46:15 +0900

    fix: health_check

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -5,7 +5,7 @@ from app.api.routes import login, users, utils, problem
 api_router = APIRouter()
 
 
-api_router.include_router(login.router, prefix="/login",tags=["login"])
+# api_router.include_router(login.router, prefix="/login",tags=["login"])
 # api_router.include_router(users.router, prefix="/users", tags=["users"])
-# api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
+api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 # api_router.include_router(problem.router, prefix="/problem", tags=["problem"])
