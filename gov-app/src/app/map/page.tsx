bfcb40532a602d835a22810344327d8d3c349df6
pkmiya <xbzd.cnlv@icloud.com>
2024-10-31 14:57:02 +0900

    add page

--- gov-app/src/app/map/page.tsx
+++ gov-app/src/app/map/page.tsx
@@ -1,6 +1,5 @@
+import { MyMap } from '@/components/MyMap';
 import { SidebarWithHeader } from '@/components/SidebarWIthHeader';
-
-import { MyMap } from '../components/MyMap';
 
 export default function Home() {
   return (
