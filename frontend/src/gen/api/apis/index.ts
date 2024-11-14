d3bc5da1383f0b685e73521057be4c73f8695de3
pkmiya <xbzd.cnlv@icloud.com>
2024-11-14 23:20:02 +0900

    add gen

---
+++ frontend/src/gen/api/apis/index.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './LoginApi';
+export * from './ProblemsApi';
+export * from './UsersApi';
---
+++ frontend/src/gen/api/models/index.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './BodyUpdateUserMe';
+export * from './HTTPValidationError';
+export * from './Msg';
+export * from './NewPassword';
+export * from './Problem';
+export * from './ProblemCreate';
+export * from './ProblemUpdate';
+export * from './Token';
+export * from './User';
+export * from './UserCreate';
+export * from './UserUpdate';
+export * from './ValidationError';
