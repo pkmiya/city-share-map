5260bde767013dda6316f98a239f9a6674ca77a9
pkmiya <xbzd.cnlv@icloud.com>
2024-11-20 15:34:42 +0900

    add env (api base url)

--- frontend/src/config/env.ts
+++ frontend/src/config/env.ts
@@ -1,4 +1,11 @@
 export const Env = {
-  accessToken: process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN ?? '',
+  apiBaseUrl: process.env.NEXT_PUBLIC_API_BASE_URL ?? '',
   envLiffId: process.env.NEXT_PUBLIC_LIFF_ID ?? '',
+  mapboxAccessToken: process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN ?? '',
+};
+
+export const MockEnv = {
+  apiBaseUrl: 'http://localhost:8000',
+  envLiffId: 'dummyId',
+  mapboxAccessToken: 'dummyToken',
 };
