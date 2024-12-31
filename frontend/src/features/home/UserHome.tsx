8a5935201a26499b155579979833bfd9cddbcd0e
pkmiya <xbzd.cnlv@icloud.com>
2024-12-31 12:44:47 +0900

    fix user home style

--- frontend/src/features/home/UserHome.tsx
+++ frontend/src/features/home/UserHome.tsx
@@ -16,45 +16,46 @@ export const UserHome = () => {
       <Text fontSize="lg" fontWeight="bold" mb={4} textAlign="center">
         ホーム画面
       </Text>
-      <Box
-        bg="blue.500"
-        borderRadius={4}
-        mb={2}
-        p={4}
-        w="95%"
-        onClick={() => router.push(pagesPath.post.new.$url().pathname)}
-      >
-        <Center mb={4}>
-          <TbMessageReport color="white" size="60px" />
-        </Center>
-        <Text color="white" textAlign="center">
-          レポートを投稿する
-        </Text>
+      <Box maxW="95%">
+        <Box
+          bg="blue.500"
+          borderRadius={4}
+          mb={4}
+          p={4}
+          onClick={() => router.push(pagesPath.post.new.$url().pathname)}
+        >
+          <Center mb={4}>
+            <TbMessageReport color="white" size="60px" />
+          </Center>
+          <Text color="white" textAlign="center">
+            レポートを投稿する
+          </Text>
+        </Box>
+        <HStack gap={4}>
+          <Box
+            bg="gray.200"
+            borderRadius={4}
+            p={4}
+            onClick={() => router.push(pagesPath.map.$url().pathname)}
+          >
+            <Center mb={4}>
+              <FiMap color="gray" size="40px" />
+            </Center>
+            マップを見る
+          </Box>
+          <Box
+            bg="gray.200"
+            borderRadius={4}
+            p={4}
+            onClick={() => router.push(pagesPath.profile.$url().pathname)}
+          >
+            <Center mb={4}>
+              <LuClipboardList color="gray" size="40px" />
+            </Center>
+            自分のレポートを見る
+          </Box>
+        </HStack>
       </Box>
-      <HStack gap={4}>
-        <Box
-          bg="gray.200"
-          borderRadius={4}
-          p={4}
-          onClick={() => router.push(pagesPath.map.$url().pathname)}
-        >
-          <Center mb={4}>
-            <FiMap color="gray" size="40px" />
-          </Center>
-          マップを見る
-        </Box>
-        <Box
-          bg="gray.200"
-          borderRadius={4}
-          p={4}
-          onClick={() => router.push(pagesPath.profile.$url().pathname)}
-        >
-          <Center mb={4}>
-            <LuClipboardList color="gray" size="40px" />
-          </Center>
-          自分のレポートを見る
-        </Box>
-      </HStack>
     </VStack>
   );
 };
