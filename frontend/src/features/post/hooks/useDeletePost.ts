0f4c61ed31ac0abd51793fd788cd6cb493396c5a
pkmiya <xbzd.cnlv@icloud.com>
2025-01-11 19:41:32 +0900

    chore: remove routing

--- frontend/src/features/post/hooks/useDeletePost.ts
+++ frontend/src/features/post/hooks/useDeletePost.ts
@@ -1,17 +1,14 @@
 import { useToast } from '@chakra-ui/react';
 import { useMutation } from '@tanstack/react-query';
-import { useRouter } from 'next/navigation';
 
 import { postProblemApi } from '@/api/client';
 import { postKeys } from '@/features/auth/constants/queryKey';
-import { pagesPath } from '@/gen/$path';
 import { DeletePostRequest } from '@/gen/api';
 import queryClient from '@/lib/react-query';
 import { getErrorStatus } from '@/utils/error';
 
 export const useDeletePost = () => {
   const toast = useToast();
-  const router = useRouter();
 
   const mutation = useMutation({
     mutationFn: async (req: DeletePostRequest) => {
@@ -35,7 +32,6 @@ export const useDeletePost = () => {
       queryClient.invalidateQueries({
         queryKey: [postKeys.post, res],
       });
-      await router.push(pagesPath.map.$url().pathname);
       toast({
         duration: 2000,
         position: 'bottom-right',
