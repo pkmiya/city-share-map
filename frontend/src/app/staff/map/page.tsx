c6796f3a5545ea9f9683b1a484f7f2b7f44507a5
pkmiya <xbzd.cnlv@icloud.com>
2025-01-11 18:31:26 +0900

    apply in pages

--- frontend/src/app/staff/map/page.tsx
+++ frontend/src/app/staff/map/page.tsx
@@ -1,11 +1,11 @@
 import { SidebarWithHeader } from '@/components/SidebarWIthHeader';
-import { MyMap } from '@/features/map/MyMap';
+import { AdminMap } from '@/features/map/AdminMap';
 
 export default function Home() {
   return (
     <>
       <SidebarWithHeader>
-        <MyMap h="80vh" p={4} w="80vw" />
+        <AdminMap h="80vh" p={4} w="80vw" />
       </SidebarWithHeader>
     </>
   );
