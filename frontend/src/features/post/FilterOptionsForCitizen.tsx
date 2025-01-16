8c6d2d3cf7e9930fe901f56c5ee203f77acd9909
pkmiya <xbzd.cnlv@icloud.com>
2025-01-17 00:19:01 +0900

    change default theme and fix styling

--- frontend/src/features/post/FilterOptionsForCitizen.tsx
+++ frontend/src/features/post/FilterOptionsForCitizen.tsx
@@ -1,10 +1,16 @@
 import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
   Box,
   Checkbox,
-  HStack,
+  Flex,
   Radio,
   RadioGroup,
   Select,
+  Spacer,
   Stack,
   Text,
 } from '@chakra-ui/react';
@@ -33,55 +39,97 @@ export const FilterOptionsForCitizen = ({
 
   return (
     <Box mb={4}>
-      <Text fontSize="lg" fontWeight="bold" my={4}>
-        検索オプション
-      </Text>
-      <Box ml={4}>
-        <HStack>
-          <RadioGroup
-            value={
-              filters.isSolved === null ? '1' : filters.isSolved ? '2' : '3'
-            }
-            w="38%"
-            onChange={(value) =>
-              onFilterChange('isSolved', value === '1' ? null : value === '2')
-            }
-          >
-            <Stack direction="row" spacing={4}>
-              <Text fontWeight="bold">対応状況</Text>
-              <Radio value="1">すべて</Radio>
-              <Radio value="2">解決済</Radio>
-              <Radio value="3">未解決</Radio>
+      <Accordion allowToggle>
+        <AccordionItem>
+          <AccordionButton>
+            <Text>検索オプション</Text>
+            <Spacer />
+            <AccordionIcon />
+          </AccordionButton>
+
+          <AccordionPanel>
+            <Stack
+              direction={{
+                base: 'column',
+                xl: 'row',
+              }}
+              wrap="wrap"
+            >
+              <Flex align="center" justify="flex-start">
+                <RadioGroup
+                  alignItems="flex-start"
+                  alignSelf="center"
+                  value={
+                    filters.isSolved === null
+                      ? '1'
+                      : filters.isSolved
+                        ? '2'
+                        : '3'
+                  }
+                  w="340px"
+                  onChange={(value) =>
+                    onFilterChange(
+                      'isSolved',
+                      value === '1' ? null : value === '2',
+                    )
+                  }
+                >
+                  <Stack
+                    direction={{
+                      base: 'column',
+                      sm: 'row',
+                    }}
+                  >
+                    <Text fontWeight="bold">対応状況</Text>
+                    <Stack direction="row">
+                      <Radio value="1">すべて</Radio>
+                      <Radio value="2">解決済</Radio>
+                      <Radio value="3">未解決</Radio>
+                    </Stack>
+                  </Stack>
+                </RadioGroup>
+              </Flex>
+
+              <Spacer />
+              <Checkbox
+                isChecked={filters.userId !== null}
+                w="240px"
+                onChange={(e) =>
+                  onFilterChange('userId', e.target.checked ? myUserId : null)
+                }
+              >
+                自分の投稿のみ
+              </Checkbox>
+              <Spacer />
+              <Select
+                maxW={{
+                  base: 'full',
+                  md: '400px',
+                }}
+                minW="160px"
+                placeholder="課題を選択"
+                value={filters.problemId || ''}
+                w={{
+                  base: 'full',
+                  md: '30vw',
+                }}
+                onChange={(e) =>
+                  onFilterChange(
+                    'problemId',
+                    e.target.value ? Number(e.target.value) : null,
+                  )
+                }
+              >
+                {problems?.map((problem) => (
+                  <option key={problem.id} value={problem.id}>
+                    {problem.name}
+                  </option>
+                ))}
+              </Select>
             </Stack>
-          </RadioGroup>
-          <Checkbox
-            isChecked={filters.userId !== null}
-            w="18%"
-            onChange={(e) =>
-              onFilterChange('userId', e.target.checked ? myUserId : null)
-            }
-          >
-            自分の投稿のみ
-          </Checkbox>
-          <Select
-            placeholder="課題を選択"
-            value={filters.problemId || ''}
-            w="40%"
-            onChange={(e) =>
-              onFilterChange(
-                'problemId',
-                e.target.value ? Number(e.target.value) : null,
-              )
-            }
-          >
-            {problems?.map((problem) => (
-              <option key={problem.id} value={problem.id}>
-                {problem.name}
-              </option>
-            ))}
-          </Select>
-        </HStack>
-      </Box>
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
     </Box>
   );
 };
