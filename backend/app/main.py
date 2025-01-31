e9dc1d211c2f7b33b0e9cc21c62dba0b944b9d40
ec2 <musashi3814@gmail.com>
2025-01-31 13:41:41 +0900

    prod用設定

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,12 +21,13 @@ app = FastAPI(
 )
 
 # Set all CORS enabled origins
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+if settings.all_cors_origins:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.all_cors_origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
